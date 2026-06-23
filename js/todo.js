99d9e4736fbd9ba7c9f9d950e0862b13025a0cc9
희승ACMY <heseung0895@naver.com>
2026-06-23 11:33:07 +0900

    아마 끝

--- js/todo.js
+++ js/todo.js
@@ -15,7 +15,57 @@ function render(){
     todos.forEach((todo, index) => {
         const li = document.createElement("li") // <li></li>
         if(todo.done) li.classList.add("done")  // <li class=done></li>
+
+        const left = document.createElement("div") //<div></div>
+        left.className = "left" // <div class="left"></div>
+
+        const checkbox = document.createElement("input") // <input></input>
+        checkbox.type = "checkbox" // <input type="checkbox"></input>
+        checkbox.checked = todo.done // <input type="checkbox" checked="false"></input>
+        checkbox.addEventListener("change", () => {
+            todo.done = checkbox.checked
+            render()
+        })
+
+        const text = document.createElement("span")  // <span></span>
+        text.className = "todo-text"  // <span class="todo-text"></span>
+        text.textContent = todo.text  // <span class="todo-text">아침식사</span>
+
+        // const del_button = document.createElement("button")
+        // del_button.type = "button"
+        // del_button.clicked = todo.del
+        // del_button.addEventListener("click", function(){
+        //     reder()
+        // })        
+
+        const delBtn = document.createElement("button")
+        delBtn.type = "button"
+        delBtn.className = "delete-btn"
+        delBtn.textContent = "삭제"
+        delBtn.addEventListener("click", () => {
+            todos.splice(index, 1)
+            render()
+        })
+        
+        left.appendChild(checkbox)
+        left.appendChild(text)
+        left.appendChild(delBtn)
+
+        li.appendChild(left)
+        todoList.appendChild(li)
     })
+
+    updateCounts()
+
+}
+function updateCounts(){
+    const total = todos.length
+    const remaining = todos.filter((t) => !t.done).length
+    remainingCount.textContent = remaining
+    totoalCount.textContent = String(remaining)
+    totoalCount.textContent = String(total)
+   
+    
 }
 
 function addTodo(){
@@ -28,6 +78,7 @@ function addTodo(){
     todoInput.focus()
     
     // 화면 그리기
+    render()
 
 }
 
